1e3ced4a0d051435f9b0b49e273641ebdea245ec
Allen Wang <allen@tome.page>
2024-07-06 02:42:27 -0700

    move reduction operation overhead to the backward pass

--- npgrad/ops/reduction.py
+++ npgrad/ops/reduction.py
@@ -46,50 +46,9 @@ class _MinMaxOp(ReduceOp):
     super().__init__(x, axis, keepdims)
     self.argop = np.argmax if self.OP == 'max' else np.argmin
     self.op = np.max if self.OP == 'max' else np.min
-    
-    
-    self._cached_indices = None
-
-    self._transpose_axes = None
-    self._transpose_shape = None
-    self._nested_shape = None
 
   def forward(self):
-    if self.x.ndim == 0 or self.x.size == 0:
-      data = self.op(self.x.data, axis=self.axis, keepdims=self.keepdims)
-    else:
-      if not isinstance(self.axis, (int, tuple, list, type(None))):
-        raise ValueError(f'axis has wrong type {type(self.axis)}')
-      
-      if isinstance(self.axis, int):
-        axis = (self.axis,)
-      elif self.axis is None:
-        axis = list(range(self.x.ndim))
-      else:
-        axis = list(self.axis)
-      
-      self._transpose_axes = axis
-      self._flat_shape = [-1]
-      self._nested_shape = [self.x.shape[i] for i in axis]
-      out_shape = []
-      for i, dim in enumerate(self.x.shape):
-        if i in axis:
-          if self.keepdims:
-            out_shape.append(1)
-        else:
-          self._transpose_axes.append(i)
-          self._flat_shape.append(dim)
-          self._nested_shape.append(dim)
-          out_shape.append(dim)
-
-      x_reshaped = self.x.data.transpose(self._transpose_axes)\
-                              .reshape(self._flat_shape)
-
-      indices = self.argop(x_reshaped, axis=0, keepdims=True)
-      self._cached_indices = indices
-      data = np.take_along_axis(x_reshaped, indices, axis=0)
-      data = data.reshape(out_shape)
-
+    data = self.op(self.x.data, axis=self.axis, keepdims=self.keepdims)
     self.out = self.ndarray_cls(data, (self.x,), self)
 
     return self.out
@@ -98,19 +57,34 @@ class _MinMaxOp(ReduceOp):
     if self.x.ndim == 0 or self.x.size == 0:
       self.x.grad += self.out.grad
     else:
-      curr_grad_reshaped = self.x.grad.transpose(self._transpose_axes)\
-                                      .reshape(self._flat_shape)
-      grad_reshaped = expand_reduced_dims(
-        self.out.grad, self.x.shape,
-        self.axis, self.keepdims
-      ).transpose(self._transpose_axes).reshape(self._flat_shape)
+      if isinstance(self.axis, int):
+        axis = (self.axis,)
+      elif self.axis is None:
+        axis = list(range(self.x.ndim))
+      else:
+        axis = list(self.axis)
 
-      curr_grad = np.take_along_axis(curr_grad_reshaped, self._cached_indices, axis=0)
+      transpose_axes = axis
+      flat_shape = [-1]
+      nested_shape = [self.x.shape[i] for i in axis]
+      for i, dim in enumerate(self.x.shape):
+        if i not in axis:
+          transpose_axes.append(i)
+          flat_shape.append(dim)
+          nested_shape.append(dim)
+
+      grad_reshaped = self.out.grad.reshape(flat_shape)
+
+      x_reshaped = self.x.data.transpose(transpose_axes).reshape(flat_shape)
+      indices = self.argop(x_reshaped, axis=0, keepdims=True)
+      curr_grad_reshaped = self.x.grad.transpose(transpose_axes).reshape(flat_shape)
+      curr_grad = np.take_along_axis(curr_grad_reshaped, indices, axis=0)
+
       updated_grad = grad_reshaped + curr_grad
-      np.put_along_axis(curr_grad_reshaped, self._cached_indices, updated_grad, axis=0)
+      np.put_along_axis(curr_grad_reshaped, indices, updated_grad, axis=0)
       
-      inv_axes = np.argsort(self._transpose_axes)
-      self.x.grad = curr_grad_reshaped.reshape(self._nested_shape).transpose(inv_axes)
+      inv_axes = np.argsort(transpose_axes)
+      self.x.grad = curr_grad_reshaped.reshape(nested_shape).transpose(inv_axes)
 
 
 class Max(_MinMaxOp):
