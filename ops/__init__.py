69a16279370c684d2924794832c84c0fb6befb0a
Allen Wang <allen@tome.page>
2024-07-05 19:21:44 -0700

    update requiremens and delete files

--- ops/__init__.py
+++
@@ -1,6 +0,0 @@
-from basics import Add, Subtract, Multiply, Divide, Pow  # noqa: F401
-from index import Slice, Assign, MaskedFill, Lookup  # noqa: F401
-from reduction import Sum, Mean, Max, Min  # noqa: F401
-from shape import Tranpose,  Reshape, Swapaxes, Ravel, Squeeze, Expanddims, Concat   # noqa: F401
-from tensors import Matmul, Einsum  # noqa: F401
-from unary import Log, Exp, Sin, Cos, Tan, Relu, Tanh  # noqa: F401
